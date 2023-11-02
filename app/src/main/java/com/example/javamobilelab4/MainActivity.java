e937a7cad4d54c4635916adaa5b7732b241ec6c8
Simge Özgündöndü <simgeozgndndu@gmail.com>
2023-11-02 14:43:03 +0300

    Initial commit

---
+++ app/src/main/java/com/example/javamobilelab4/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.javamobilelab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.javamobilelab4.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnGo;
+    EditText txtAddress;
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnGo=findViewById(R.id.btnGo);
+        txtAddress=findViewById(R.id.txtAddress);
+        webView=findViewById(R.id.webView);
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webView.loadUrl("https://" + txtAddress.getText());
+            }
+        });
+        if(getIntent() != null && getIntent().getData() !=null){
+            txtAddress.setText(getIntent().getData().toString());
+            webView.loadUrl(getIntent().getData().toString());
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
